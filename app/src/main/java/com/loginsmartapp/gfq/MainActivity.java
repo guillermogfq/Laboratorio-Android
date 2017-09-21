3e03661fbd6fcc348f581543ed474b952bb16c97
Guillermo Fuentes <guillermofuentesquijada@gmail.com>
2017-09-21 13:23:11 -0300

    cambios 3

--- app/src/main/java/com/loginsmartapp/gfq/MainActivity.java
+++ app/src/main/java/com/loginsmartapp/gfq/MainActivity.java
@@ -1,14 +1,22 @@
 package com.loginsmartapp.gfq;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE = 1;
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +29,32 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                iniciarCamara();
             }
         });
+        imageView = (ImageView) findViewById(R.id.imagephoto);
 
         TextView nombre = (TextView) findViewById(R.id.nombres);
         TextView apellidos = (TextView) findViewById(R.id.apellidos);
 
         nombre.setText(getIntent().getExtras().getString("nombres"));
         apellidos.setText(getIntent().getExtras().getString("apellidos"));
+    }
+
+    private void iniciarCamara() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 
 }
